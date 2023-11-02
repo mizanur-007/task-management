76dec7471a4f05bd9317c72c46638b9c219699be
MD Mizanur Rahman <mizanridoy007@gmail.com>
2023-11-02 11:48:52 +0600

    set a  observer

--- src/AuthProvider/AuthProvider.jsx
+++ src/AuthProvider/AuthProvider.jsx
@@ -1,19 +1,36 @@
-import React, { Children, createContext } from "react";
+import React, { Children, createContext, useEffect, useState } from "react";
 import auth from "../Firebase/firebase.config";
-import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { createUserWithEmailAndPassword, onAuthStateChanged, updateProfile } from "firebase/auth";
 
 export const AuthContext = createContext(null);
 
 const AuthProvider = ({ children }) => {
+
+    const [user, setUser] = useState("");
+
+    //create a user with email password
   const signIn = (email,password)=>{
     return createUserWithEmailAndPassword(auth, email, password)
   }
+
+  //set users name and image 
 
   const updateUser = (name,image)=>{
     updateProfile(auth.currentUser, {
         displayName: name, photoURL: image
       })
   }
+
+    // observer 
+    useEffect(() => {
+        const unSubscribe = onAuthStateChanged(auth, (user) => {
+          setUser(user);
+        //   setLoading(false);
+        });
+        return () => {
+          unSubscribe();
+        };
+      }, []);
 
   const authInfo ={
     signIn,
